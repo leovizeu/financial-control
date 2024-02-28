29ede3d71d5c6fdeb3309b6ceab3ced1700fb913
leovizeu <leovizeu@hotmail.com>
2024-02-27 22:55:35 -0300

    add current balance and income

---
+++ app/Components/CurrentBalance.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CurrentBalance = () => {
+  return (
+    <div className='p-10 uppercase'>
+        <h1>
+            Current Balance
+        </h1>
+        <p className='text-blue-500 text-xl py-3'>
+            R$
+        </p>
+    </div>
+  )
+}
+
+export default CurrentBalance
