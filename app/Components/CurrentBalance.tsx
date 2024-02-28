a279afafc5dd64b1fa26a316148385f91de696ac
leovizeu <leovizeu@hotmail.com>
2024-02-27 23:32:03 -0300

    add styling

--- app/Components/CurrentBalance.tsx
+++ app/Components/CurrentBalance.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const CurrentBalance = () => {
   return (
-    <div className='w-full px-10 py-5 uppercase'>
+    <div className='w-full py-5 uppercase'>
         <h1>
             Current Balance
         </h1>
-        <p className='text-blue-500 text-xl py-3'>
-            R$
+        <p className='text-blue-500 font-sans text-3xl py-3'>
+            R$ 4000.00
         </p>
     </div>
   )
--- app/Components/Income.tsx
+++ app/Components/Income.tsx
@@ -6,14 +6,14 @@ const Income = () => {
         <div className='flex justify-around'>
             <div className='w-full flex-col flex items-center justify-center'>
                 <h1 className='uppercase'>Income</h1>
-                <p className='text-green-500 text-xl'>R$</p>
+                <p className='text-green-500 font-sans text-xl'>R$ 4000.00</p>
             </div>
             
         </div>
         <div className='flex justify-between'>
             <div className='w-full flex-col flex items-center justify-center'>
                 <h1 className='uppercase'>Expences</h1>
-                <p className='text-red-500 text-xl'>R$</p>
+                <p className='text-red-500 font-sans text-xl'>R$ 0.00</p>
             </div>
         </div>
     </div>
